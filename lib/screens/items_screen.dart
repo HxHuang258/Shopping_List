20d26877a20ce0a5659152276779cfb98c2abd97
Harry Huang <psyhh4@exmail.nottingham.ac.uk>
2024-06-29 16:45:56 +0100

    212 - Form Aware drop down button

--- lib/screens/items_screen.dart
+++ lib/screens/items_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/screens/new_item_screen.dart';
 import 'package:shopping_list/widgets/grocery_list.dart';
 
 class ItemsScreen extends StatefulWidget {
@@ -12,10 +13,21 @@ class ItemsScreen extends StatefulWidget {
 }
 
 class _ItemsScreenState extends State<ItemsScreen> {
+void _addItem() {
+  Navigator.of(context).push(
+    MaterialPageRoute(builder: (ctx) => const NewItemScreen()),
+  );
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: _addItem, 
+            icon: const Icon(Icons.add)),
+        ],
         title: const Text('Your Groceries'),
       ),
       body: GroceryList(groceryItems: groceryItems),
