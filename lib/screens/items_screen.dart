b8d0e680c3cee899685c3dd751c5d763ddc3ddc1
Harry Huang <psyhh4@exmail.nottingham.ac.uk>
2024-07-08 20:35:45 +0100

    228 - Avoiding unneccessary requests
    So when you add a new Item, and firebase returns a response with an ID, you then use that ID to add that same item to your local grocery items list instead of requesting firebase again in the load items method

--- lib/screens/items_screen.dart
+++ lib/screens/items_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/screens/new_item_screen.dart';
 import 'package:shopping_list/widgets/grocery_list.dart';
 import 'package:shopping_list/models/grocery_item.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ItemsScreen extends StatefulWidget {
   const ItemsScreen({super.key});
@@ -14,12 +16,36 @@ class ItemsScreen extends StatefulWidget {
 }
 
 class _ItemsScreenState extends State<ItemsScreen> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-12ca3-default-rtdb.europe-west1.firebasedatabase.app',
+        'shopping-list.json');
+
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body); //convert json data back to list of maps
+    final List<GroceryItem> _loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries.firstWhere((catItem) => catItem.value.title == item.value['category']).value;
+      _loadedItems.add(GroceryItem(id: item.key, name: item.value['name'], quantity: item.value['quantity'], category: category));
+    }
+    setState(() {
+      _groceryItems = _loadedItems;
+    });
+  }
+
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(builder: (ctx) => const NewItemScreen()),
     );
-
+    
     if (newItem == null) {
       return;
     }
